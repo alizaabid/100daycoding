45245be8578e3ee9b4da0451721db4c5265c8251
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-22 13:26:28 +0500

    push on git

---
+++ day3.ts
@@ -0,0 +1,30 @@
+//question 7
+
+let addition1: number= 5;
+let addition2: number= 5;;
+console.log(addition1+addition2);
+
+let substraction1: number= 15;
+let substraction2: number= 5;
+console.log(substraction1-substraction2);
+
+let multiplication1: number= 5;
+let multiplication2: number= 2;
+console.log(multiplication1*multiplication2);
+
+let division1: number= 20;
+let division2: number= 2;
+console.log(division1/division2);
+
+//question 8
+console.log(4*2);
+console.log(4/2);
+console.log(4+2);
+console.log(4-2);
+
+//question
+let favNumber: number= 5;
+console.log(`my favourite number is ${favNumber} because it's my birth date.`);
+
+
+
---
+++ day4.js
@@ -0,0 +1,18 @@
+//question 10
+/*this
+is
+a
+comment
+question
+example*/
+//question 11
+let myFriends = ["saima", "sabahat", "arisha"];
+console.log(myFriends[0]);
+console.log(myFriends[1]);
+console.log(myFriends[2]);
+//question 12 
+for (let myFriend of myFriends) {
+    console.log(`assalam o alikum ${myFriend},  I am inviting you on my grand birthday party dinner.`);
+}
+;
+export {};
