901c8dc30c04dd2297f29182990447e68d0e9edb
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-24 22:04:56 +0500

    push

---
+++ day6.ts
@@ -0,0 +1,36 @@
+//question 16
+let friends:string[] = ["abid","muhammad","aliza"];
+console.log("good nnews, I arrange bigger dinner and more guests & friends.")
+
+friends.unshift("sabahat","arisha","hussain");
+friends.splice(friends.length / 2,0,"hasina");
+friends.push("sanobia");
+
+friends.forEach(friend=>{console.log(`dear friends ${friend} would you come my grand dinner party.`)});
+
+//question 17
+console.log("unfortunately, I can only two people for dinner")
+
+while(friends.length>2){let removedFriend = friends.pop()
+console.log(`sorry, ${removedFriend} , I can't invite you for dinner`)}
+
+friends.splice(0, friends.length);
+console.log(friends);
+
+//question 18
+let places:string[]=["saudia arabia", 'dubai', 'japan', 'kashmir', 'china']
+
+console.log("original order:", places)
+console.log("alphabetical order:", [...places].sort())
+console.log("original order:", places)
+console.log("reverse alphabetical order:", [...places].reverse())
+console.log("original order:", places)
+places.reverse();
+console.log("reverse order:", places)
+places.reverse();
+console.log("original order:", places)
+places.sort();
+console.log("alphabetical order:",places)
+places.reverse();
+console.log("original order:", places)
+
