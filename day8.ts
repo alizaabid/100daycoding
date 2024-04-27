7a0569733ab94f6f235207969dfdcbf064486ce6
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-27 23:28:46 +0500

    push

---
+++ day8.ts
@@ -0,0 +1,38 @@
+//question 22
+
+let friends:string[] = ["sabahat","arisha","hussain","hasina","sanobia"]
+console.log(friends[2]);
+
+
+//question 23
+let car ="prado";
+console.log("I car=='prado'? I predict true")
+console.log(car == "prado")
+console.log("I car=='pajeero'? I predict false")
+console.log(car=="pajeero")
+
+//question 24
+console.log("testing equality with string:");
+console.log("apple" == "apple");
+
+
+console.log("testing with lower case:")
+console.log("Apple".toLowerCase()=="apple")
+
+
+console.log("numerical test");
+console.log(15>5);
+console.log(21<11);
+
+
+console.log("test with 'and' and 'or':")
+console.log(true && false)
+console.log(true || false)
+
+
+let vegetable:String[]=["potato","tomato","onion"]
+console.log("Is potato in'vegetable'?");
+console.log(vegetable.includes("potato"));
+
+console.log("Is tomato not in'vegetable'?");
+console.log(vegetable.includes("tomato"));
